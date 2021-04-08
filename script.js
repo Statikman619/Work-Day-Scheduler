33082627964b4d50a10c493bcede81464322d11b
Statikman619 <martinez.michael02@gmail.com>
2021-04-07 22:55:54 -0700

    added some JS

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let timeblocksEl = $(".time-block");
 let timeDisplayEl = $("#time-display");
 
 function todaysDate() {
-    let thisDay = moment().format("MMM DD, YYYY");
-    currentDateEl.text("Today's Date: " + thisDay)
-    timeblocksEl.each(function() {
-
-    })
+  let thisDay = moment().format("MMM DD, YYYY hh:mm:ss");
+  timeDisplayEl.text("Today's Date: " + thisDay);
+  timeblocksEl.each(function () {});
+}
+setInterval(todaysDate, 1000);
