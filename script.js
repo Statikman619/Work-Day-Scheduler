2dca822b9cb30e3fb90d05a4180777735168b84c
Statikman619 <martinez.michael02@gmail.com>
2021-04-08 17:39:28 -0700

    added more JS

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ function todaysDate() {
   let thisDay = moment().format("MMM DD, YYYY hh:mm:ss");
   timeDisplayEl.text("Today's Date: " + thisDay);
   timeblocksEl.each(function () {});
+  let currentHour = moment().hour();
+  if (currentHour === $(this).data().time) {
+    $(this).children("textarea").addClass("present");
+  }
 }
 setInterval(todaysDate, 1000);
+
+function colorCode() {
+  let currentHour = moment().hour();
+}
